7998d117cdceecb08b12c1eaa1eed452a561f61f
Bar <110189471+Eze-leky@users.noreply.github.com>
2025-01-26 22:12:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@
                 message1.classList.add('fadeIn');
             }, 10000); // 10000 milisegundos = 10 segundos
 
-            // Obtener el día actual (0 = Domingo, 1 = Lunes, ..., 6 = Sábado)
-            const currentDay = new Date().getDay();
-
+            // Obtener el día y la hora actuales
+            const now = new Date();
+            const currentDay = now.getDay(); // 0 = Domingo, 4 = Jueves
+            const currentHour = now.getHours(); // Hora en formato 24 horas
+              
             // Mostrar segundo mensaje solo si es jueves (4) o domingo (0)
             setTimeout(function() {
                 const message1 = document.getElementById('message1');
                 message1.style.display = 'none'; // Ocultar el primer mensaje
 
-                if (currentDay === 0 || currentDay === 4) { // Si es domingo o jueves
+               if ((currentDay === 0 || currentDay === 4) && currentHour >= 19) {
                     const message2 = document.getElementById('message2');
                     message2.style.display = 'block';
                     message2.classList.add('fadeIn');
